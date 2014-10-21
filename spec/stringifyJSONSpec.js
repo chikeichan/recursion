60f25601e031e44152b30fdeb55f7260a3cf97e8
Chi Kei Chan <chikeichan@gmail.com>
2014-10-21 00:18:55 -0700

    Finished stringifyJSON

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -5,12 +5,32 @@ describe('stringifyJSON', function(){
     stringifiableObjects.forEach(function(test){
       var result = stringifyJSON(test);
       var expected = JSON.stringify(test);
+
+       
+      //FOR DEBUGGING
+      /*
+      console.log('Input    : '+test);
+      console.log('Expected : '+expected);
+      console.log('Actual   : '+result);
+      console.log('---------------------');
+      */
+
       expect(result).to.equal(expected);
     });
 
     unstringifiableValues.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
+
+       
+      //FOR DEBUGGING
+      /*
+      console.log('Input    : '+obj);
+      console.log('Expected : '+expected);
+      console.log('Actual   : '+result);
+      console.log('---------------------');
+      */
+
       expect(result).to.equal(expected);
     });
 
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,40 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var answer = [];
+
+  if (obj === null) {
+  	answer += 'null';
+  } else if (typeof obj === 'number') {
+  	answer += obj.toString();
+  } else if (typeof obj === 'boolean') {
+  	answer += obj ? 'true' : 'false';
+  } else if (typeof obj === 'string') {
+  	answer += '"';
+  	answer += obj;
+  	answer += '"';
+  } else if (Array.isArray(obj)){
+  	answer += '[';
+  	_.each(obj, function(x,i){
+  		answer += stringifyJSON(x);
+  		answer += i!==obj.length-1 ? ',': '';
+  	});
+  	answer += ']';
+  } else if (typeof obj === 'object'){
+  	var lastKeys = Object.keys(obj)[[Object.keys(obj).length-1]];
+  	answer += '{';
+  	_.each(obj, function(x,i){
+  		if (typeof x !== 'function' && typeof x !== 'undefined') {
+  			answer += stringifyJSON(i);
+  			answer += ':';
+  			answer += stringifyJSON(x);
+  			answer += i!==lastKeys ? ',' : '';
+  		}
+  	});
+  	answer += '}';
+  } else if (typeof obj === 'function') {
+  	answer+= '';
+  }
+
+  return answer;
 };
